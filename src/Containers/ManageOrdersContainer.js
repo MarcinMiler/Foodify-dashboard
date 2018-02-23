2a2ca368c628307f8307530d503c5d42b0d4bd48
Marcin <marcin4951@gmail.com>
2018-02-23 18:36:05 +0100

    New order subscription

--- src/Containers/ManageOrdersContainer.js
+++ src/Containers/ManageOrdersContainer.js
@@ -6,6 +6,10 @@ import ManageOrders from '../Components/ManageOrders'
 
 class ManageOrdersContainer extends Component {
 
+    componentDidMount() {
+        this.subscribe()
+    }
+
     updateStatus = (newStatus, id) => {
         this.props.updateOrderStatus({
             variables: { newStatus, id },
@@ -19,9 +23,21 @@ class ManageOrdersContainer extends Component {
         })
     }
 
+    subscribe = () => {
+        return this.props.currentOrders.subscribeToMore({
+            document: newOrderSubscription,
+            updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) return prev
+                
+                return Object.assign({}, prev, {
+                    currentOrders: [ ...prev.currentOrders, subscriptionData.data.newOrder ]
+                })
+            }
+        })
+    }
+
     render() {
         const { currentOrders } = this.props.currentOrders
-        console.log(currentOrders)
         return(
             <div>
                 { currentOrders && <ManageOrders currentOrders={currentOrders} updateStatus={this.updateStatus} /> }
@@ -48,6 +64,21 @@ const currentOrdersQuery = gql`
 const updateOrderStatusMutation = gql`
     mutation updateOrderStatus($newStatus: String! $id: ID!) {
         updateOrderStatus(newStatus: $newStatus id: $id)
+    }
+`
+const newOrderSubscription = gql`
+    subscription newOrder {
+        newOrder {
+            id,
+            date,
+            orderStatus,
+            products {
+                productID,
+                quantity
+            }
+            totalPrice,
+            address
+        }
     }
 `
 export default compose(
--- src/apollo.js
+++ src/apollo.js
@@ -2,6 +2,9 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 // import { setContext } from 'apollo-link-context'
+import { WebSocketLink } from 'apollo-link-ws'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' })
 
@@ -15,8 +18,24 @@ const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' })
 //     }
 // })
 
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/subscriptions`,
+    options: {
+      reconnect: true
+    }
+})
+
+const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query)
+      return kind === 'OperationDefinition' && operation === 'subscription'
+    }, 
+    wsLink,
+    httpLink,
+)
+
 export const client = new ApolloClient({
     // link: authLink.concat(httpLink),
-    link: httpLink,
+    link,
     cache: new InMemoryCache(),
 })
